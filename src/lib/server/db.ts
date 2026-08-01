1767cb52d22a294753f2c4013a376191ddde3c39
varccy <vmarcicreator@gmail.com>
2026-08-01 16:30:58 +0200

    Update db.ts

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,19 +1,20 @@
-import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { config } from 'dotenv'
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
+import type { PrismaClient as PrismaClientType } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { config } from 'dotenv';
 
-config()
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+config();
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClientType };
 
 const createPrismaClient = () => {
-  const connectionString = process.env.DATABASE_URL
-  const pool = new Pool({ connectionString })
-  const adapter = new PrismaPg(pool)
-  return new PrismaClient({ adapter })
-}
+  const connectionString = process.env.DATABASE_URL;
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
+  return new PrismaClient({ adapter });
+};
 
-export const db: PrismaClient = globalForPrisma.prisma ?? createPrismaClient()
+export const db: PrismaClientType = globalForPrisma.prisma ?? createPrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
-
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
